97f757ce42b80fd603952108d2dd95db025d5403
henilsarang <henilsarang2716@gmail.com>
2020-07-30 00:46:15 -0400

    git ignore modified

---
+++ build/precache-manifest.7a058e1ec138baa89fd55dfd6b74e303.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "98bba8b8cbb3bea340e8",
+    "url": "/static/css/main.d869db2c.chunk.css"
+  },
+  {
+    "revision": "98bba8b8cbb3bea340e8",
+    "url": "/static/js/main.d8a94ea0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b76049058449c9ba7b8b",
+    "url": "/static/css/2.90bc96ad.chunk.css"
+  },
+  {
+    "revision": "b76049058449c9ba7b8b",
+    "url": "/static/js/2.f4d6987c.chunk.js"
+  },
+  {
+    "revision": "7709c10e8c30753537d94a6b69d7542a",
+    "url": "/static/media/aboutBcg.7709c10e.jpeg"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/static/media/mainBcg.984874d1.jpeg"
+  },
+  {
+    "revision": "160fddff3ca71887ebf8da3929244379",
+    "url": "/static/media/productsBcg.160fddff.jpeg"
+  },
+  {
+    "revision": "945afb997e0731b038ed58fadacfeb3f",
+    "url": "/static/media/contactBcg.945afb99.jpeg"
+  },
+  {
+    "revision": "9675841294261b773267d557f531a8ff",
+    "url": "/static/media/singleProductBcg.96758412.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "/static/media/storeBcg.46143c2f.jpeg"
+  },
+  {
+    "revision": "0819a81634391f8518fe325378b8f994",
+    "url": "/static/media/defaultBcg.0819a816.jpeg"
+  },
+  {
+    "revision": "2881507b84dc990ebacd967b3749f6a9",
+    "url": "/static/media/logo.2881507b.svg"
+  },
+  {
+    "revision": "9547c1ae00c364365c19e7974c6721bb",
+    "url": "/index.html"
+  }
+];
